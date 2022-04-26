723e5041a9a1ad892b20cb209c6276d8f6101086
Juan Fernandes <juandanieldosr@gmail.com>
2022-04-26 23:06:05 +0100

    ,

--- Seçoes feitas por mim/09-Data-Structures-Operators/starter/script.js
+++ Seçoes feitas por mim/09-Data-Structures-Operators/starter/script.js
@@ -55,9 +55,107 @@ const restaurant = {
 ///////////////////////////////////////
 // Looping Objects: Object Keys, Values, and Entries
 
+//property Names
+const properties = Object.keys(openingHours);
+console.log(properties); // resultado ("thu" , "fri" , "sat")
+// console.log(`We are open on ${properties.length} days`); // resultado array com ("thu" , "fri" , "sat")
+
+let openStr = `We are open on ${properties.length} days: `; // resultado array com ("thu" , "fri" , "sat")
+
 for (const day of Object.keys(openingHours)) {
-  console.log(day); // thu,fri,sat
+  console.log(day); // thu,fri,sat que sao os key names
 }
+
+console.log(openStr); // we are open in 3 days
+
+// Property Values
+
+const values = Object.values(openingHours);
+console.log(values); //imprime o objeto tudo
+
+//Entire object
+const entries = Object.entries(openingHours)
+console.log(entries); // transformou o object em array
+
+// for(const x of entries) {
+//    console.log(x) // print each key and each value
+// }
+
+// [key, value]
+for(const [day,{open, close}] of entries) {
+
+  console.log (`On ${day} we open at ${open} and close at ${close}`) //On thu we open at 12 and close at 22
+}
+
+
+///Coding Challenge #2
+
+// Let's continue with our football betting app! Keep using the 'game' variable from
+// before.
+// Your tasks:
+// 1. Loop over the game.scored array and print each player name to the console,
+// along with the goal number (Example: "Goal 1: Lewandowski")
+// 2. Use a loop to calculate the average odd and log it to the console (We already
+// studied how to calculate averages, you can go check if you don't remember)
+// 3. Print the 3 odds to the console, but in a nice formatted way, exactly like this:
+// Odd of victory Bayern Munich: 1.33
+// Odd of draw: 3.25
+// Odd of victory Borrussia Dortmund: 6.5
+// Get the team names directly from the game object, don't hardcode them
+// (except for "draw"). Hint: Note how the odds and the game objects have the
+// same property names 😉
+// 4. Bonus: Create an object called 'scorers' which contains the names of the
+// players who scored as properties, and the number of goals as the value. In this
+// game, it will look like this:
+// {
+// Gnarby: 1,
+// Hummels: 1,
+// Lewandowski: 2
+// }
+// GOOD LUCK 😀
+
+const game = {
+    team1: 'Bayern Munich',
+    team2: 'Borrussia Dortmund',
+    players: [
+      [
+        'Neuer',
+        'Pavard',
+        'Martinez',
+        'Alaba',
+        'Davies',
+        'Kimmich',
+        'Goretzka',
+        'Coman',
+        'Muller',
+        'Gnarby',
+        'Lewandowski',
+      ],
+      [
+        'Burki',
+        'Schulz',
+        'Hummels',
+        'Akanji',
+        'Hakimi',
+        'Weigl',
+        'Witsel',
+        'Hazard',
+        'Brandt',
+        'Sancho',
+        'Gotze',
+      ],
+    ],
+    score: '4:0',
+    scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+    date: 'Nov 9th, 2037',
+    odds: {
+      team1: 1.33,
+      x: 3.25,
+      team2: 6.5,
+    },
+  };
+
+
 
 // //////////////////////////////////////
 // // Optional Chaining
@@ -403,46 +501,46 @@ for (const day of Object.keys(openingHours)) {
 // Then, call the function again with players from game.scored
 // GOOD LUCK �
 
-const game = {
-  team1: 'Bayern Munich',
-  team2: 'Borrussia Dortmund',
-  players: [
-    [
-      'Neuer',
-      'Pavard',
-      'Martinez',
-      'Alaba',
-      'Davies',
-      'Kimmich',
-      'Goretzka',
-      'Coman',
-      'Muller',
-      'Gnarby',
-      'Lewandowski',
-    ],
-    [
-      'Burki',
-      'Schulz',
-      'Hummels',
-      'Akanji',
-      'Hakimi',
-      'Weigl',
-      'Witsel',
-      'Hazard',
-      'Brandt',
-      'Sancho',
-      'Gotze',
-    ],
-  ],
-  score: '4:0',
-  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
-  date: 'Nov 9th, 2037',
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
-  },
-};
+// const game = {
+//   team1: 'Bayern Munich',
+//   team2: 'Borrussia Dortmund',
+//   players: [
+//     [
+//       'Neuer',
+//       'Pavard',
+//       'Martinez',
+//       'Alaba',
+//       'Davies',
+//       'Kimmich',
+//       'Goretzka',
+//       'Coman',
+//       'Muller',
+//       'Gnarby',
+//       'Lewandowski',
+//     ],
+//     [
+//       'Burki',
+//       'Schulz',
+//       'Hummels',
+//       'Akanji',
+//       'Hakimi',
+//       'Weigl',
+//       'Witsel',
+//       'Hazard',
+//       'Brandt',
+//       'Sancho',
+//       'Gotze',
+//     ],
+//   ],
+//   score: '4:0',
+//   scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+//   date: 'Nov 9th, 2037',
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
 
 ///////////////////////////////////////
 // Coding Challenge #1
